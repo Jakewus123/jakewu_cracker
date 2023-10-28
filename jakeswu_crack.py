ef759dac886cbf03620ac78b3349f91b4dd3c427
Jakewus123 <147282448+Jakewus123@users.noreply.github.com>
2023-10-28 10:46:08 +0800

    Add files via upload

--- jakeswu_crack.py
+++ jakeswu_crack.py
@@ -143,10 +143,10 @@ def sarfraz():
     elif select =='9':
        cutter()
     elif select =='W':
-        os.system('xdg-open https://www.facebook.com/profile.php?id=100090514260120)
+        os.system('xdg-open https://www.facebook.com/profile.php?id=100090514260120')
         pass
     elif select =='F':
-        os.system('xdg-open https://www.facebook.com/profile.php?id=100090514260120)
+        os.system('xdg-open https://www.facebook.com/profile.php?id=100090514260120')
     else:
         print('\n Select valid option ... ')
         time.sleep(2)
